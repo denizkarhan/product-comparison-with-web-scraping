db09b6e6b1208ac9dde300678d9502c083d6eb62
erencalbay <73295969+erencalbay@users.noreply.github.com>
2022-10-23 14:45:13 +0300

    Add files via upload
    Gözat ve detay sayfaları güncellendi.

--- movieapp/movies/admin.py
+++ movieapp/movies/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from .models import Category, DataSetDeneme
+from .models import Category, DataSetDeneme, Data, Data2, Data3
 
 # Register your models here.
 
 admin.site.register(DataSetDeneme)
 admin.site.register(Category)
+admin.site.register(Data)
+admin.site.register(Data2)
+admin.site.register(Data3)
--- movieapp/movies/views.py
+++ movieapp/movies/views.py
@@ -1,24 +1,24 @@
 from django.shortcuts import render
-from .models import Category, DataSetDeneme
+from .models import Category, DataSetDeneme, Data, Data2
 
 # Create your views here.
 
 def home(request):
     data = {
         "kategoriler": Category.objects.all(),
-        "filmler": DataSetDeneme.objects.all()
+        "notebooklar": Data2.objects.all()
     }
     return render(request, "index.html", data)
 
 def movies(request):
     data = {
         "kategoriler": Category.objects.all(),
-        "filmler": DataSetDeneme.objects.all()
+        "notebooklar": Data2.objects.all(),
     }
     return render(request, "movies.html", data)
 
 def moviedetails(request, id):
     data = {
-        "laptoplar": DataSetDeneme.objects.get(id=id)
+        "laptoplar": Data2.objects.get(id=id)
     }
     return render(request, "details.html", data)
