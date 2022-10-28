d2cb4374efc18f78ee1215297c63b8f4698a6044
Deniz Karhan <81527587+denizkarhan@users.noreply.github.com>
2022-10-29 00:16:10 +0300

    Delete movieapp directory

--- movieapp/magaza/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Data3, Category, Data, magaza
-
-# Register your models here.
-
-admin.site.register(Category)
-admin.site.register(Data3)
-admin.site.register(Data)
-admin.site.register(magaza)
--- movieapp/movies/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Category, Data, Data2, Data3, Data4
-
-# Register your models here.
-
-admin.site.register(Category)
-admin.site.register(Data2)
-admin.site.register(Data3)
-admin.site.register(Data4)
