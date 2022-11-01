12f20739d59a6bd43293d4982746982d458084b1
erencalbay <73295969+erencalbay@users.noreply.github.com>
2022-11-01 20:10:14 +0300

    Add files via upload
    magaza eklendi

---
+++ movieapp/magaza/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import Data3, Category, Data, magaza, Data6
+
+# Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Data3)
+admin.site.register(Data)
+admin.site.register(magaza)
+admin.site.register(Data6)
